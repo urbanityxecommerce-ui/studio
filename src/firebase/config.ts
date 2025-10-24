fbd08f166454f5bdad0de0c26cbc43eeddcf2a2e
sucharita Mukherjee <urbanityxecommerce@gmail.com>
2025-10-24 21:21:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8009113417-ffa8d",
+  "appId": "1:563434074045:web:693da2bad6d5cb4bf92cd4",
+  "apiKey": "AIzaSyAhXSZHju5BNl6JPhroaLXDW_sQKCrA0vw",
+  "authDomain": "studio-8009113417-ffa8d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "563434074045"
+};
