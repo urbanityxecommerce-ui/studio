ea7ce14d13c42ef97b5e92764703cda0d9879736
sucharita Mukherjee <urbanityxecommerce@gmail.com>
2025-10-24 21:59:41 +0000

    Again failed pls tell me do I have to do anything it should work now and

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
-  },
   images: {
     remotePatterns: [
       {
