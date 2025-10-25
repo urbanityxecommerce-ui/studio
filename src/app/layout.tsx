92781abacb981305323aaf60b98cb63f65e64606
sucharita Mukherjee <urbanityxecommerce@gmail.com>
2025-10-25 14:33:23 +0000

    ye btao ki ise mujhe mobile or desktop dono me work mujhe app me convert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 export const metadata: Metadata = {
   title: 'CreatorX SEO',
   description: 'The ultimate SEO toolkit for YouTubers and Instagram creators.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -21,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
