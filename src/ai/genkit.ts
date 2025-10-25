b32f09c41d88182eb1430bc077b3b93b1817a036
sucharita Mukherjee <urbanityxecommerce@gmail.com>
2025-10-25 15:47:41 +0000

    yar avi v api call nhi krparha hai bar bar app deploy kese krugi pls shi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {genkit, configureGenkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
   model: 'googleai/gemini-2.5-flash',
 });
