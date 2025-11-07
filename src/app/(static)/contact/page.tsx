ae40e7254922d8176d51fb418226493af718d835
sucharita Mukherjee <urbanityxecommerce@gmail.com>
2025-11-07 13:43:01 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/(static)/contact/page.tsx
+++ src/app/(static)/contact/page.tsx
@@ -1,10 +1,12 @@
+
 "use client";
 
+import * as React from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
