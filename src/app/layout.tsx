b8925ca66dccf1f164e94485768f963527ea3125
sucharita Mukherjee <urbanityxecommerce@gmail.com>
2025-12-04 13:50:40 +0000

    you didnot applied

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.Node;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
